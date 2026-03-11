eba3fa7b2d7e49c6a927787bc69fe149371b607a
1224100827mrs-gif <1224100827.mrs@gmail.com>
2026-03-10 19:53:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
 from flask import Flask, request, jsonify
-
 app = Flask(__name__)
-
-@app.route('/convertir-temperatura', methods=['POST'])
-def convertir_temperatura():
+@app.route('/promedio', methods=['POST'])
+def calcular_promedio():
     datos = request.get_json()
-    
-    valor = datos['valor']
-    escala_origen = datos['escala'].upper()  
-
-    if escala_origen == 'C':
-        resultado = (valor * 9/5) + 32
-        escala_destino = 'F'
-    elif escala_origen == 'F':
-        resultado = (valor - 32) * 5/9
-        escala_destino = 'C'
-    else:
-        return jsonify({"error": "Escala no válida. Usa 'C' o 'F'."}), 400
+    nombre = datos['nombre']
+    calificaciones = datos['calificaciones']
+    promedio = sum(calificaciones) / len(calificaciones)
 
     respuesta = {
-        "valor_original": valor,
-        "escala_origen": escala_origen,
-        "resultado": round(resultado, 2),
-        "escala_destino": escala_destino
+    "nombre": nombre,
+    "promedio": promedio
     }
-    
     return jsonify(respuesta)
-
 if __name__ == '__main__':
     app.run(debug=True)
